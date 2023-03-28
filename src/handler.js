16aea604b7da40793306beaef28e60316f5887b6
Aditya Irvan <adityairvanku@gmail.com>
2023-03-28 18:48:53 +0700

    add addBookHandler and fix bug

--- src/handler.js
+++ src/handler.js
@@ -1,57 +1,67 @@
 const { nanoid } = require('nanoid');
 const books = require('./books');
 
-const id = nanoid(16);
-const insertedAt = new Date().toISOString();
-const updatedAt = insertedAt;
-let finished = false;
 const addBookHandler = (request, h) => {
-  if (request.payload.name !== undefined) {
-    const {
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      reading,
-    } = request.payload;
-    if (readPage <= pageCount) {
-      if (readPage === pageCount) {
-        finished = true;
-      }
-    } else {
-      const response = h.response({
-        status: 'fail',
-        message:
-          'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-      });
-      response.code(400);
-      return response;
-    }
-
-    // const newBook = {
-    //   id,
-    //   name,
-    //   year,
-    //   author,
-    //   summary,
-    //   publisher,
-    //   pageCount,
-    //   readPage,
-    //   finished,
-    //   reading,
-    //   insertedAt,
-    //   updatedAt,
-    // };
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
   }
-  const response = h.response({
-    status: 'fail',
-    message: 'Gagal menambahkan buku. Mohon isi nama buku',
-  });
-  response.code(400);
-  return response;
+  if (pageCount > readPage) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+  const book = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+  books.push(book);
+
+  const isSuccess = books.filter((b) => b.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
 };
 
 module.exports = { addBookHandler };
